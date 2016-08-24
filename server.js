f2dddb5a65b937a29d9a27fda8d080b043865697
Steven McConomy <srmcconomy@gmail.com>
2016-08-24 17:06:00 -0400

    mm

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express();
 require('ejs')
 
 const bingoGenerator = require('./7x7gen')
-const bingoList = require('./goal-list')
+
+const defaults = {
+  game: 'oot',
+  mode: 'normal',
+  size: 7
+}
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
@@ -11,17 +16,20 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  var size = +req.query.size;
+  let size = +req.query.size || defaults.size;
   if (size > 200) res.send('size < 200 pls')
   if (size % 2 === 0) res.send('size must be odd')
-  var seed = +req.query.seed;
-  var board = bingoGenerator(bingoList.normal, {size, seed})
+  let seed = +req.query.seed || Math.ceil(999999 * Math.random());
+  let mode = req.query.mode || defaults.mode;
+  let game = req.query.game || defaults.game;
+  let bingoList = require(`./${game}/goal-list`)[mode];
+  let board = bingoGenerator(bingoList, {size, seed})
   res.render('index', {size, seed, board})
 })
 
 app.get('/bingo-popout', (req, res) => {
-  var size = +req.query.size;
-  var row = req.query.rowName;
+  let size = +req.query.size || defaults.size;
+  let row = req.query.rowName;
   let goals = []
   for (let i = 0; i < size; i++) {
     goals[i] = req.query['goal' + i];
